8d952ffe43d0e29e4527ded72b88a5bac43b10e2
Ruxaxup <Ruxaxup@192.168.1.112>
2016-02-10 16:17:41 -0600

    Starting GUI - Main Frame

--- src/elementosdiscretos/ElementosDiscretos.java
+++ src/elementosdiscretos/ElementosDiscretos.java
@@ -5,6 +5,8 @@
  */
 package elementosdiscretos;
 
+import vista.MainFrame;
+
 /**
  *
  * @author Ruxaxup
@@ -15,7 +17,8 @@ public class ElementosDiscretos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MainFrame frame = new  MainFrame();
+        frame.setVisible(true);
     }
     
 }
